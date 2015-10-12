508eb557013d510fe5179322e0376c1db56b8766
Frank Smith <labiniboes+git@gmail.com>
2015-10-13 01:11:09 +0200

    Take steps to control what can be null and what can't.

--- src/main/java/soy/frank/dtos/Student.java
+++ src/main/java/soy/frank/dtos/Student.java
@@ -1,12 +1,19 @@
 package soy.frank.dtos;
 
+
 public class Student {
     private final String firstName;
+    @Nullable
     private final String lastName;
 
-    public Student(String firstName, String lastName) {
+    private Student(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public static Student create(String firstName, String lastName) {
+        if (firstName == null) throw new IllegalArgumentException("Null firstName");
+        return new Student(firstName, lastName);
     }
 
     public String firstName() {
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,6 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import soy.frank.dtos.Student;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -6,11 +8,20 @@ import static org.hamcrest.core.Is.is;
 
 public class StudentTest {
 
+    @Rule
+    public ExpectedException expectedExceptionRule = ExpectedException.none();
+
     @Test
     public void should_be_able_to_create_student() {
-        Student student = new Student("Frank", "Smith");
+        Student student = Student.create("Frank", "Smith");
         assertThat(student.firstName(), is("Frank"));
         assertThat(student.lastName(), is("Smith"));
+    }
+
+    @Test
+    public void should_not_allow_us_to_create_a_student_with_a_null_name() {
+        expectedExceptionRule.expect(IllegalArgumentException.class);
+        Student.create(null, "Dracula");
     }
 
 }
