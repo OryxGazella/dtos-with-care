e3b1f44fca1166905be03be70a881c55fa6d09c4
Frank Smith <labiniboes+git@gmail.com>
2015-10-13 01:47:18 +0200

    Add an immutable data structure with lombok @data annotation.

--- src/main/java/soy/frank/dtos/Nullable.java
+++ src/main/java/soy/frank/dtos/Nullable.java
@@ -1,8 +1,4 @@
 package soy.frank.dtos;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.SOURCE)
 public @interface Nullable {
 }
--- src/main/java/soy/frank/dtos/Student.java
+++ src/main/java/soy/frank/dtos/Student.java
@@ -1,44 +1,27 @@
 package soy.frank.dtos;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
 
-import java.util.Objects;
+@AutoValue
+public abstract class Student {
 
-public class Student {
-    private final String firstName;
-    @Nullable
-    private final String lastName;
-
-    private Student(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    Student() {
     }
 
-    public static Student invalidStudent = new Student("Invalid", "Invalid");
+    public static Student invalidStudent = new AutoValue_Student("Invalid", "Invalid");
 
-    public static Student create(String firstName, String lastName) {
+    @JsonCreator
+    public static Student create(
+            @JsonProperty("firstName") String firstName,
+            @JsonProperty("lastName") String lastName) {
         if (firstName == null) return invalidStudent;
-        return new Student(firstName, lastName);
+        return new AutoValue_Student(firstName, lastName);
     }
 
-    public String firstName() {
-        return firstName;
-    }
+    public abstract String firstName();
 
-    public String lastName() {
-        return lastName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return Objects.equals(firstName, student.firstName) &&
-                Objects.equals(lastName, student.lastName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName);
-    }
+    @Nullable
+    public abstract String lastName();
 }
