ade294afb32e2930177c3525dad5be3f702c22cb
Frank Smith <oryx.gazella@gmail.com>
2015-11-09 02:46:22 +0200

    Configure as gradle project
    Update to Kotlin Beta

--- src/main/kotlin/soy/frank/dtos/Student.kt
+++ src/main/kotlin/soy/frank/dtos/Student.kt
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class Student(val firstName: String, val lastName: String?) {
     companion object {
         @JsonCreator
+        @JvmStatic
         fun create(
                 @JsonProperty("firstName") firstName: String?,
                 @JsonProperty("lastName") lastName: String?): Student {
@@ -13,6 +14,6 @@ data class Student(val firstName: String, val lastName: String?) {
             return Student(firstName, lastName)
         }
 
-        val invalidStudent = Student("Invalid", "Invalid")
+        @JvmField val invalidStudent = Student("Invalid", "Invalid")
     }
 }
